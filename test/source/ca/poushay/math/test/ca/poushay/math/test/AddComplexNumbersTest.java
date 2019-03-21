655868a68a8599944d74512cfe798693c18316b2
colin poushay <cpoushay@gmail.com>
2019-03-21 13:48:03 -0300

    can now add complex numbers

--- test/source/ca/poushay/math/test/ca/poushay/math/test/AddComplexNumbersTest.java
+++ test/source/ca/poushay/math/test/ca/poushay/math/test/AddComplexNumbersTest.java
@@ -11,5 +11,28 @@ public class AddComplexNumbersTest {
     public void zeroPlusZero() {
 
         assertEquals(new ComplexNumber(0), new ComplexNumber(0).add(new ComplexNumber(0)));
+
     }
+
+    @Test
+    public void iPlusZero() {
+        assertEquals(new ComplexNumber(0.,1), new ComplexNumber(0.,1).add(new ComplexNumber(0)));
+
+    }
+    @Test
+    public void iPlusOne() {
+        assertEquals(new ComplexNumber(1.,1), new ComplexNumber(0.,1).add(new ComplexNumber(1)));
+
+    }
+
+    @Test
+    public void iPlusI() {
+        assertEquals(new ComplexNumber(0.,1), new ComplexNumber(0.,1).add(new ComplexNumber(0,1)));
+
+    }
+
+    @Test
+    public void complexNumberPlusComplexNumber() {
+        assertEquals(new ComplexNumber(7.,7.5), new ComplexNumber(4.3,6).add(new ComplexNumber(2.7,1.5)));
+        }
 }
